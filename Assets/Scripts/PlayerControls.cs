533cadd34ed4278b74cc37db700b7af743427cd0
Goldenlion5648 <coboudinot@gmail.com>
2020-09-06 20:04:42 -0400

    forgot to save sample scene

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -6,20 +6,22 @@ public class PlayerControls : MonoBehaviour
 {
     public GameObject normalBulletPrefab;
     // Start is called before the first frame update
+    float fixedFramesPerSecond = 50;
     float bulletShootCooldown = 0;
-    float maxBulletCooldown = 2;
+    float maxBulletCooldown = 1.5f;
     void Start()
     {
-
+        maxBulletCooldown *= fixedFramesPerSecond;
     }
 
     void playerMovement()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && bulletShootCooldown == 0)
+        if (Input.GetKey(KeyCode.Space) && bulletShootCooldown <= 0)
         {
             var bullet = Instantiate(normalBulletPrefab, gameObject.transform.position, Quaternion.identity);
             bulletShootCooldown = maxBulletCooldown;
             Destroy(bullet.gameObject, 5);
+
 
 
         }
@@ -27,23 +29,32 @@ public class PlayerControls : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             gameObject.transform.position -= new Vector3(0, 0, 3) * Time.deltaTime;
-
         }
         if (Input.GetKey(KeyCode.S))
         {
             gameObject.transform.position += new Vector3(0, 0, 3) * Time.deltaTime;
-
         }
         if (Input.GetKey(KeyCode.A))
         {
             gameObject.transform.position += new Vector3(3, 0, 0) * Time.deltaTime;
-
         }
         if (Input.GetKey(KeyCode.D))
         {
             gameObject.transform.position -= new Vector3(3, 0, 0) * Time.deltaTime;
-
         }
+
+    }
+
+    private void FixedUpdate()
+    {
+
+
+        cooldowns();
+    }
+
+    void cooldowns()
+    {
+        bulletShootCooldown = Mathf.Max(0, bulletShootCooldown - 1);
 
     }
 
