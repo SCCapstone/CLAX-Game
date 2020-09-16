068330a931828938b786325d6c0451a1893b201a
Woodcastle <x.b.sidney@gmail.com>
2020-09-15 20:26:52 -0400

    moving stuff around

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public GameObject spawnPoint;
+    bool respawn = false;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = (GameObject)Instantiate(player, Vector3.zero, Quaternion.identity);
+        player.transform.position = spawnPoint.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
