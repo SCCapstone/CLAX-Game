f8eb26a23b7457a11cfe3ef96394fb6896a11e6b
landin3thorsted <54644686+landin3thorsted@users.noreply.github.com>
2020-09-23 22:28:19 -0400

    Working on the falling platform
    Colter and Landin working together to figure out how to make the falling platform fall after getting stepped on

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FallingPlatform : MonoBehaviour
+{
+    [Header("Falling")]
+    public bool collisionFall = true;
+
+    public float timingFall = 600;
+    bool isFalling = false;
+    private float downSpeed = 0;
+
+
+    // Start is called before the first frame update
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            isFalling = true;
+    }
+
+
+    //private void OnTriggerStay(Collider collision)
+    //{
+
+    //}
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFalling)
+        {
+            downSpeed += Time.deltaTime;
+            transform.parent.position = new Vector3(transform.position.x,
+                transform.position.y - downSpeed,
+                transform.position.z);
+        }
+        //timingFall -= Time.deltaTime;
+    }
+}
