fdcdb74df4405089cd3eb01f855877fe1973db81
Arleycht <47849356+Arleycht@users.noreply.github.com>
2021-02-17 23:21:42 -0500

    Update timerOptionText.cs

--- Assets/Scripts/timerOptionText.cs
+++ Assets/Scripts/timerOptionText.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class timerOptionText : MonoBehaviour
+public class TimerOptionText : MonoBehaviour
 {
     private TextMeshProUGUI textBoxGUI;
     public GameObject timerObject;
