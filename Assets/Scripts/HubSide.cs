1e3471edafc93c8e1d8a64f9fe54a0475cb72e3d
Woodcastle <x.b.sidney@gmail.com>
2021-02-21 14:07:54 -0500

    a-door-able

---
+++ Assets/Scripts/HubSide.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HubSide : MonoBehaviour
+{
+    public string sceneName;
+    bool spawnEnabled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    OnTriggerEnter(Collider other)
+    {
+        //TODO add boss is dead logic
+        if(Collider.Tag == "Player")
+        {
+            SceneManager.LoadSceneAsync(sceneName);
+        }
+    }
+}
