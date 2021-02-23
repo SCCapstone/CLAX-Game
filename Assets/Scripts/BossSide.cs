0c0ba7a0b708731e07b2f34c213f02822b3f0875
Woodcastle <x.b.sidney@gmail.com>
2021-02-23 14:52:30 -0500

    Hub door functionality, boss doors need way to tell boss died

--- Assets/Scripts/BossSide.cs
+++ Assets/Scripts/BossSide.cs
@@ -1,23 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BossSide : MonoBehaviour
 {
     [Header("Prefabs")]
-    public GameObject spawnPoint;
+    public GameObject doorway;
     public GameObject player;
+    public GameObject Boss;
+    public string sceneName;
+
+    public Material change;
+
+    private Renderer renderer;
+    
+    private bool active;
 
     // Start is called before the first frame update
     void Start()
     {
+        active = false;
+        renderer = doorway.GetComponentInChildren<Renderer>();
         player = (GameObject)Instantiate(player, Vector3.zero, Quaternion.identity);
-        player.transform.position = spawnPoint.transform.position;
+        player.transform.position = doorway.transform.GetChild(0).position;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(Boss.GetComponent<AliveObject>().health <= 0.0f)
+        {
+            active = true;
+            renderer.material = change;
+        } 
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Object entered");
+        //TODO add boss is dead logic
+        if (other.gameObject.CompareTag("Player") && active)
+        {
+            Debug.Log("Player Entered");
+            SceneManager.LoadSceneAsync(sceneName);
+        }
+    }
+
 }
---
+++ Assets/Scripts/HubSide.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HubSide : MonoBehaviour
+{
+    public string sceneName;
+    bool spawnEnabled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Object entered");
+        //TODO add boss is dead logic
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player Entered");
+            SceneManager.LoadSceneAsync(sceneName);
+        }
+    }
+}
