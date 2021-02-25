0dcb60ab36cfb45368ca4e0f285641f73f173509
Arleycht <47849356+Arleycht@users.noreply.github.com>
2021-02-25 14:15:51 -0500

    Move comment

--- Assets/Scripts/PlayerProjectile.cs
+++ Assets/Scripts/PlayerProjectile.cs
@@ -3,22 +3,22 @@
 public class PlayerProjectile : Projectile
 {
     public float damage = 0;
+
+    // TODO: Get enemy layer by name or constant? Magic numbers are legitimately scary.
+    // Enemy layer is 9
     public int enemyLayerNum;
 
     private void OnTriggerEnter(Collider other)
     {
-        //Debug.Log("Triggered");
-
-        // 9 is the layer id for enemy
         if (other.gameObject.layer == enemyLayerNum)
         {
-            Debug.Log("hit enemy");
+            //Debug.Log("hit enemy");
+
             AliveObject enemy = other.gameObject.GetComponent<AliveObject>();
 
             enemy.Damage(damage);
 
             Destroy(gameObject);
         }
-
     }
 }
