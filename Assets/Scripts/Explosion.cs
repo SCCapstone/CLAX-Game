37eb962eeaba2c18c4ffc5b499195f07f8fb5e2a
landin3thorsted <landin3thorsted@gmail.com>
2021-02-26 13:13:47 -0500

    Add explosion shield

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -81,6 +81,11 @@ public class Explosion : MonoBehaviour
             Destroy(gameObject);
         }
 
+        if (other.transform.name.Contains("cubeAttack") || other.transform.name.Contains("TriangleBossAttack"))
+        {
+            Destroy(other.gameObject);
+        }
+
     }
 
     // Update is called once per frame
