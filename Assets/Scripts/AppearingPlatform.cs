8a7359a3b064b7e93a3d33778ad956f4107114d8
Woodcastle <x.b.sidney@gmail.com>
2021-04-20 17:37:23 -0400

    fixing issue 81

---
+++ Assets/Scripts/AppearingPlatform.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearingPlatform : MonoBehaviour
+{
+    public GameObject platform;
+
+    private Renderer platformRenderer;
+    private BoxCollider platformCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platformRenderer = platform.GetComponentInChildren<Renderer>();
+        platformCollider = platform.GetComponentInChildren<BoxCollider>();
+        if (!globals.won)
+        {
+            platformRenderer.enabled = false;
+            platformCollider.enabled = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    /*
+    void Update()
+    {
+        if(globals.won && !platformRenderer.enabled)
+        {
+            platformRenderer.enabled = true;
+            platformCollider.enabled = true;
+        }
+        
+    }
+    */
+}
