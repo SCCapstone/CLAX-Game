f6b7c1dafeffd2d63b87762e9bcec5881f5d7aa2
landin3thorsted <54644686+landin3thorsted@users.noreply.github.com>
2021-04-26 14:11:17 -0400

    Add comments to Explosion attack

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -1,6 +1,11 @@
 ﻿using System.Collections;
 using UnityEngine;
-
+/**
+ * This file takes care of the logic for the second attack
+ * The second attack creates a black orb that grows, shrinks, 
+ * then dissapears. If a boss or door is touched by the orb it 
+ * will take damage.
+ */
 public class Explosion : MonoBehaviour
 {
     public float growTime;
@@ -8,14 +13,20 @@ public class Explosion : MonoBehaviour
 
     public float damage;
 
-    // Start is called before the first frame update
+    /**
+     * Start is called before the first frame update
+     */
     void Start()
     {
         StartCoroutine(Grow());
 
         Destroy(gameObject, growTime * 2);
     }
-
+    /**
+     * This method takes care of growing the attack by using the game 
+     * time grow for a set amount of time, by changing the scale of the
+     * object
+     */
     private IEnumerator Grow()
     {
         Vector3 startScale = transform.localScale;
@@ -36,6 +47,11 @@ public class Explosion : MonoBehaviour
         StartCoroutine(Shrink());
     }
 
+    /**
+     * This method takes care of shrink the attack by using the game 
+     * time shrink for a set amount of time, by changing the scale of the
+     * object
+     */
     private IEnumerator Shrink()
     {
         Vector3 startScale = transform.localScale;
@@ -56,9 +72,13 @@ public class Explosion : MonoBehaviour
         Destroy(gameObject);
     }
 
+    /**
+     * This method takes care of the collisions for the orb and other
+     * objects sucs as the boss and dorrs. Damage will be applied and 
+     * 
+     */
     private void OnTriggerEnter(Collider other)
     {
-        // 9 is the layer id for enemy
         if (other.gameObject.layer == Globals.enemyLayerNum)
         {
             AliveObject enemy = other.gameObject.GetComponent<AliveObject>();
